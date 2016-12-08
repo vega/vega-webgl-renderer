95317fbe98a91555a56216024a3da079d5f3f584
Jeffrey Baumes <jeff.baumes@kitware.com>
2016-12-08 09:04:27 -0500

    Fixup package for using vanilla vega master

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {CanvasHandler} from 'vega-scenegraph';
+import {CanvasHandler, renderModule} from 'vega-scenegraph';
 import {default as WebGLRenderer} from './src/WebGLRenderer';
 
 // Patch CanvasHandler
@@ -6,9 +6,6 @@ CanvasHandler.prototype.context = function() {
   return this._canvas.getContext('2d') || this._canvas._textCanvas.getContext('2d');
 };
 
-if (window && window.vega) {
-  window.vega.addCustomRenderer('webgl', CanvasHandler, WebGLRenderer);
-}
+renderModule('webgl', {handler: CanvasHandler, renderer: WebGLRenderer});
 
-// export {default as WebGLRenderer} from './src/WebGLRenderer';
 export {WebGLRenderer};
